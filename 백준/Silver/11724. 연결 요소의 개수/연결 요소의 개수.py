b623d6240c757b499103b1a3885bfac297244762
Byeonghun Kim - 김병훈 <114432005+ByeongHun-Kim@users.noreply.github.com>
2023-01-18 15:27:34 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 776 ms, Memory: 65000 KB -BaekjoonHub

---
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+
+N, M = map(int, sys.stdin.readline().split())
+graph = [[] for _ in range(N+1)]
+visited = [False for _ in range(N+1)]
+
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+
+def dfs(v):
+    visited[v] = True
+    for i in graph[v]:
+        if not visited[i]:
+            dfs(i)
+
+
+result = 0
+for n in range(1, N+1):
+    if not visited[n]:
+        dfs(n)
+        result += 1
+
+print(result)
